02b72580be0f45b71138c62895109d5cc0cbdcab
elliot <elliot@wyrick.ws>
2026-07-30 19:10:26 -0400

    main.c time import & test

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <lgpio.h>
 
+//grab the current time from linux kernal
+#include <time.h>
+//for sleep
+#include <unistd.h>
 int main() {
-	printf("meow");
-	return 1;
+
+	for(int i = 0; i < 15; i++){
+	
+		time_t raw_time = time(NULL);
+	
+		printf("the time is: %s", ctime(&raw_time));
+		//sleep 1s
+		sleep(1);
+	}
+	
+return 1;
 }
